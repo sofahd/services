1dbc24b85358ee0131f9da56169f99d9506aeb41
m0 <mo>
2024-02-15 00:35:45 +0100

    feat: full representation of the LogApi as a Service

---
+++ services/log_api.py
@@ -0,0 +1,32 @@
+from sofahutils import DockerComposeService
+
+class LogApiService(DockerComposeService):
+    """
+    This class is used to represent the log-api service. It is a subclass of the DockerComposeService class.
+    """
+
+    def __init__(self, name:str, port:int, log_folder_path:str, token:str) -> None:
+        """
+        Constructor for the LogApiService class.
+        """
+        service_def = [
+            "    container_name: <name>",
+            "    restart: unless-stopped",
+            "    build: ",
+            "      context: ./log-api",
+            "      args:",
+            "        WAITRESS_PORT: <port>",
+            "    networks:",
+            "      - log_net",
+            "    environment:",
+            "      - PYTHONUNBUFFERED=1",
+            "    volumes:",
+            "      - <path>:/home/api/log_data"
+        ]
+
+        variables = {
+            "<name>": name,
+            "<port>": port,
+            "<path>": log_folder_path
+        }
+        super().__init__(name=name, service_def=service_def, github_link="https://$TOKEN:x-oauth-basic@github.com/sofahd/log-api.git", token=token, networks=["log_net"], variables=variables)
