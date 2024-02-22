596fec013e535bc1500dd36d0e9c74a0437e3cd6
m0 <mo>
2024-02-22 11:17:44 +0100

    feat: bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="services",
-    version="0.1",
+    version="0.2",
     description="Services for sofah",
     long_description=long_description,
     long_description_content_type="text/markdown",
