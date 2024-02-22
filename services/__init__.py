4fcf08aa86768da2f7bbfbe10fe30c877320cc97
m0 <mo>
2024-02-22 21:36:14 +0100

    feat: make EnnormService available for ez import

--- services/__init__.py
+++ services/__init__.py
@@ -3,3 +3,4 @@ from .log_api import LogApiService
 from .api_honeypot import ApiHoneypot
 from .nginx_honeypot import NginxHoneypot
 from .recon import ReconService
+from .ennorm import EnnormService
